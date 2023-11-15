a6f63956e5dd62e12e4544c30136e0f59df5e065
Clamaxfr <144500526+Clamaxfr@users.noreply.github.com>
2023-11-15 20:15:24 +0100

    New Commit

--- project/baballe/baballe.js
+++ project/baballe/baballe.js
@@ -1,19 +1,14 @@
 "use strict";
 
 export default class Baballes{
-    vv = Math.random()<0.5?20:-20; // vitesse vertical
-    vh = Math.random()<0.5?20:-20; // vitesse horizontal
+    vv = Math.random()<0.5?20:-20; 
+    vh = Math.random()<0.5?20:-20;
     r = 20;
 constructor(canvas, x, y) {
 let c1 = Math.floor(Math.random()*255)
 let c2 = Math.floor(Math.random()*255)
 let c3 = Math.floor(Math.random()*255)
 
-// let x1 = Math.floor(Math.random()*(canvas.width - this.r*2) + this.r)
-
-// let y1 = Math.floor(Math.random()*(canvas.height - this.r*2) +this.r)
-//  this.x = x1
-//     this.y = y1
 this.x = x;
 this.y = y;
     this.c = `rgb(${c1} ,${c2} ,${c3})`
@@ -23,7 +18,6 @@ console.log(`${c1} ,${c2} ,${c3}`);
     dessin(canvas) {
         const ctx = canvas.getContext("2d")
         ctx.fillStyle = this.c
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
         ctx.fill();
